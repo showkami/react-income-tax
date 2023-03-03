8e13db6656ee5049eefcf9680ded727a2ae8f1d1
Masashi Hasegawa <mhasegawa.net@gmail.com>
2023-03-04 00:07:16 +0900

    wholeStatementを所得shotokuと控除kojoに分割

--- src/App.js
+++ src/App.js
@@ -8,123 +8,111 @@ import Tax from "./4_tax/tax";
 
 function App() {
 
-    const [wholeStatement, setWholeStatement] = useState({
-        shotoku: {
-            // 所得項目キー: {"shunyu": 収入, "shotoku": 所得, ....}
-            kyuyo: {
-                "shunyu": undefined,  // 給与収入
-                "kyuyo_shotoku_kojo": undefined,
-                "shotoku": undefined,  // 給与所得
-            },
+    const [shotoku, setShotoku] = useState({
+        // 所得項目キー: {"shunyu": 収入, "shotoku": 所得, ....}
+        kyuyo: {
+            "shunyu": undefined,  // 給与収入
+            "kyuyo_shotoku_kojo": undefined,
+            "shotoku": undefined,  // 給与所得
+        },
+    });
+
+    const [kojo, setKojo] = useState({
+        // 所得控除項目キー: {"is_applicable": T/F, "kojo_fromShotokuTax": 所得税からの控除額, "kojo_fromJuminTax": 住民税からの控除額, ....}
+        zasson: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        iryo_hi: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        shakai_hokenryo: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        shokibo_kiryo_kyosai: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        seimei_hokenryo: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        jishin_hokenryo: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        kifukin: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        shougaisha: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        kafu: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        hitorioya: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        kinro_gakusei: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        haigusha_tokubetsu: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        fuyou: {
+            "is_applicable": false,
+            "kojo_fromShotokuTax": undefined,
+            "kojo_fromJuminTax": undefined,
+        },
+        kiso: {
+            "is_applicable": true,
+            "kojo_fromShotokuTax": 480_000,
+            "kojo_fromJuminTax":   430_000,
         },
 
-        kojo: {
-            // 所得控除項目キー: {"is_applicable": T/F, "kojo_fromShotokuTax": 所得税からの控除額, "kojo_fromJuminTax": 住民税からの控除額, ....}
-            zasson: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            iryo_hi: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            shakai_hokenryo: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            shokibo_kiryo_kyosai: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            seimei_hokenryo: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            jishin_hokenryo: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            kifukin: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            shougaisha: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            kafu: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            hitorioya: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            kinro_gakusei: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            haigusha_tokubetsu: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            fuyou: {
-                "is_applicable": false,
-                "kojo_fromShotokuTax": undefined,
-                "kojo_fromJuminTax": undefined,
-            },
-            kiso: {
-                "is_applicable": true,
-                "kojo_fromShotokuTax": 480_000,
-                "kojo_fromJuminTax":   430_000,
-            },
-
-        }
     });
-    console.log("App() called. wholeStatement=", wholeStatement);
 
-
-    function changeShotoku (newShotoku) {
-        let newWholeStatement = {...wholeStatement};
-        newWholeStatement.shotoku = newShotoku;
-        setWholeStatement(newWholeStatement);
-    }
-    function changeKojo (newKojo) {
-        let newWholeStatement = {...wholeStatement};
-        newWholeStatement.kojo = newKojo;
-        setWholeStatement(newWholeStatement);
-    }
+    console.log("App() called. shotoku=", shotoku, ", kojo=", kojo);
 
     return (
         <div className="App">
             <ShotokuHokenryoEasyInput  />
 
             <Shotoku
-                shotokuData={wholeStatement.shotoku}
-                changeShotoku={changeShotoku}
+                shotokuData={shotoku}
+                changeShotoku={setShotoku}
             />
 
             <SonekiTsusan />
 
             <KazeiShotoku
-                kojoData={wholeStatement.kojo}
-                changeKojo={changeKojo}
+                kojoData={kojo}
+                changeKojo={setKojo}
             />
 
             <Tax
-                taxData={wholeStatement.tax}
+                shotoku={shotoku}
+                kojo={kojo}
             />
         </div>
     );
