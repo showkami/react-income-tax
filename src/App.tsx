f99a44cda2832c08b831172486ad56175d44eff2
Masashi Hasegawa <mhasegawa.net@gmail.com>
2024-08-18 03:01:59 +0900

    custom Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Box } from "@mui/material";
 import { IncomeDict, IncomeInput } from "./01_IncomeInput/Income";
 import { incomeTypes } from "./01_IncomeInput/IncomeTypes";
 import { AggregationOfProfitLoss } from "./02_AggregationOfProfitLoss/AggregationOfProfitLoss";
@@ -15,6 +9,7 @@ import {
 } from "./03_DeductionsFromIncome/DeductionsFromIncome";
 import { deductionTypes } from "./03_DeductionsFromIncome/DeductionTypes";
 import { InputFromSalaryStatement } from "./00_InputFromSalaryStatement/InputFromSalaryStatement";
+import { Accordion } from "./component/Accordion";
 
 function App() {
   // 所得計算用state
@@ -37,48 +32,39 @@ function App() {
 
   return (
     <Box width={"100%"}>
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          給与明細からインプット
-        </AccordionSummary>
-        <AccordionDetails>
+      <Accordion
+        title={"給与明細からインプット"}
+        content={
           <InputFromSalaryStatement setSalaryRevenue={setSalaryRevenue} />
-        </AccordionDetails>
-      </Accordion>
+        }
+        defaultExpanded={true}
+      />
 
-      <Accordion defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          所得の計算
-        </AccordionSummary>
-        <AccordionDetails>
+      <Accordion
+        title={"所得の計算"}
+        content={
           <IncomeInput
             incomeDict={incomeDict}
             setIncomeDict={setIncomeDict}
             salaryRevenue={salaryRevenue}
           />
-        </AccordionDetails>
-      </Accordion>
+        }
+      />
 
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          損益通算及び損失の繰越控除
-        </AccordionSummary>
-        <AccordionDetails>
-          <AggregationOfProfitLoss />
-        </AccordionDetails>
-      </Accordion>
+      <Accordion
+        title={"損益通算及び損失の繰越控除"}
+        content={<AggregationOfProfitLoss />}
+      />
 
-      <Accordion>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          課税所得の計算 (所得控除の反映)
-        </AccordionSummary>
-        <AccordionDetails>
+      <Accordion
+        title={"課税所得の計算(所得控除の反映)"}
+        content={
           <DeductionsFromIncome
             deductionSourceDict={deductionSourceDict}
             setDeductionSourceDict={setDeductionSourceDict}
           />
-        </AccordionDetails>
-      </Accordion>
+        }
+      />
     </Box>
   );
 }
