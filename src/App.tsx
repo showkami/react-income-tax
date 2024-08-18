27ec20402c0a755e4a7231c95960cf063ea915d8
Masashi Hasegawa <mhasegawa.net@gmail.com>
2024-08-18 23:48:17 +0900

    基礎控除対応

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,9 @@ function App() {
   // 所得控除の元となる数値state
   const initDeductionsDict: DeductionsDict = Object.fromEntries(
     deductionTypes.map((deductionType) => {
-      return [deductionType.id, { forIncomeTax: 0, forResidentTax: 0 }];
+      return deductionType.id === "basic"
+        ? [deductionType.id, { forIncomeTax: 480_000, forResidentTax: 430_000 }] // 基礎控除はあらかじめ金額を入れておく。TODO: 税額計算のロジックに関わることなので、ここに書くべきではない気がする。。
+        : [deductionType.id, { forIncomeTax: 0, forResidentTax: 0 }];
     }),
   );
   const [deductionsDict, setDeductionsDict] =
